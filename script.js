6d32c7006fbfd574805aa2132d4f3227addeb79e
agmbio <95878252+agmbio@users.noreply.github.com>
2022-02-04 15:37:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ var counter=0;
 function jump(){
     if(character.classList == "animate"){return}
     character.classList.add("animate");
-    setTimeout(function(){
-        character.classList.remove("animate");
-    },300);
 }
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
